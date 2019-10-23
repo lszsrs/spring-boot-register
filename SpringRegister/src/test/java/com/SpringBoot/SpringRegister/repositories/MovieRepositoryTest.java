5b646a2505fa003de81f5758067fb401a9151a8b
imistless <larissa.souza.soares@outlook.com>
2019-10-23 21:26:25 -0200

    Refatoração e teste unitário

--- SpringRegister/src/test/java/com/SpringBoot/SpringRegister/repositories/MovieRepositoryTest.java
+++ SpringRegister/src/test/java/com/SpringBoot/SpringRegister/repositories/MovieRepositoryTest.java
@@ -1,5 +1,40 @@
 package com.SpringBoot.SpringRegister.repositories;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.SpringBoot.SpringRegister.models.Director;
+import com.SpringBoot.SpringRegister.models.Movie;
+
+
+@Transactional
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class MovieRepositoryTest {
 
+	@Autowired
+	private MovieRepository movieRepository; 
+	private Movie movie; 
+	private Director director;
+	
+	@Before	
+	public void setup() {
+		director = new Director("William Wyler", "xxx", "xxx", "xxxx");
+		movie = new Movie("Roman Holiday", 1, "October 5, 1953", "xxx", director);				
+	}
+	
+	@Test
+	public void saveDirector() {
+		movieRepository.save(movie); 
+		Movie movieFound = movieRepository.getOne(movie.getId());
+		assertEquals(movie, movieFound); 
+	}
+	
 }
